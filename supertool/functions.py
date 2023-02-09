923e66e419e0fb78a5eda3f5f8d9d7bcf44278e4
Álvaro GR <4166199+alvgalrus@users.noreply.github.com>
2023-02-09 16:50:10 +0100

    Add timeout to gateway detection and try default

--- supertool/functions.py
+++ supertool/functions.py
@@ -152,16 +152,22 @@ def detectrouterip(r,host):
     gw = getgateways()
     for a in gw:
         print("[detect_router]: Checking if gateway ({}) is a ZTE F8648P router".format(a))
-        z = r.get("http://{}/".format(a))
-        if '<span id="pdtVer">&#70;&#56;&#54;&#52;&#56;&#80;</span>' in z.text:
-            print("[detect_router]: It looks like {} is a ZTE F8648P router".format(a))
-            return(a)
+        try:
+            z = r.get("http://{}/".format(a), timeout=5)
+        except:
+            print("[detect_router]: Timeout waiting for gateway")
+        else:
+            if '<span id="pdtVer">&#70;&#56;&#54;&#52;&#56;&#80;</span>' in z.text:
+                print("[detect_router]: It looks like {} is a ZTE F8648P router".format(a))
+                return(a)
 
         print("[detect_router]: No match. Falling back to manual router IP input")
         print("What is the router IP? [default: {}]".format(host))
         routerip = str(input())
         if routerip != '':
             return(routerip)
+        else:
+            return(host)
 
 def checkopenport():
     if 3339 in [i.laddr.port for i in psutil.net_connections()]:
